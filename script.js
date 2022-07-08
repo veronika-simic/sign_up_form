eb250d1902b7c3005d26b2faf6263b2d612a9c2f
veronika-simic <veronikasimic21@gmail.com>
2022-07-08 10:59:28 +0200

    Finish project

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container");
-const signUpBtn = document.querySelector('.red-bg button');
+const signUpBtn = document.querySelector(".red-bg button");
 
 signUpBtn.addEventListener('click', function(){
-    container.classList.toggle('change');
+    container.classList.toggle("change");
 })
